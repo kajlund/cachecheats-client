5587e47975257a73199f4ce2e158ccbf4777ab44
Kaj Lund <kaj.lund@gmail.com>
2023-03-11 18:32:56 +0200

    Adds outline style to Link component

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -3,9 +3,12 @@
   export let href = '#'
   export let icon = ''
   export let iconLeft = '1'
+  export let style = ''
+
+  let styles = 'link ' + style
 </script>
 
-<div class="link">
+<div class={styles}>
   <a {href}>
     {#if iconLeft ===  '1'}
       <div class="link-content">
@@ -30,18 +33,32 @@
     display: flex;
     align-items: center;
   }
-
   .link a:hover {
     color: var(--col-primary-dk);
   }
-
   .link-content {
+    margin: 0;
+    padding: 0 0.8rem;
     display: flex;
     align-items: center;
   }
-
   .icon-left,
   .icon-right {
     font-size: 2rem;
+  }
+  .outline {
+    background: transparent;
+    color: var(--col-primary);
+    border: 2px solid var(--col-primary);
+    border-radius: 0.5rem;
+    font-weight: bold;
+    box-shadow: none;
+  }
+  .outline:hover,
+  .outline:active {
+    background: transparent;
+    color: var(--col-primary-dk);
+    border-color: var(--col-primary-dk);
+    box-shadow: 1px 1px 8px rgba(77, 51, 51, 0.26);
   }
 </style>
