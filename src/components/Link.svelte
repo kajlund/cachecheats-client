a6e029d57f5b2f46d1cf1e2bca5af71274cc1d81
Kaj Lund <kaj.lund@gmail.com>
2023-03-11 17:29:44 +0200

    Adds generic link component

---
+++ src/components/Link.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let caption = 'Link'
+  export let href = '#'
+  export let icon = ''
+  export let iconLeft = '1'
+</script>
+
+<div class="link">
+  <a {href}>
+    {#if iconLeft ===  '1'}
+      <div class="link-content">
+        <div class="icon icon-left">
+          <i class="material-symbols-rounded">{icon}</i>
+        </div>
+        {caption}
+      </div>
+    {:else}
+      <div class="link-content">
+        {caption}
+        <span class="icon icon-right">
+          <i class="material-symbols-rounded">{icon}</i>
+        </span>
+      </div>
+    {/if}
+  </a>
+</div>
+
+<style>
+  .link {
+    display: flex;
+    align-items: center;
+  }
+
+  .link a:hover {
+    color: var(--col-primary-dk);
+  }
+
+  .link-content {
+    display: flex;
+    align-items: center;
+  }
+
+  .icon-left,
+  .icon-right {
+    font-size: 2rem;
+  }
+</style>
